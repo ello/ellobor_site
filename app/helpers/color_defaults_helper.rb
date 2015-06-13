d84673bb04b737e6fcc6691dd9dfdd8afebbc7fa
gblakeman <grant.blakeman@gmail.com>
2015-06-13 16:32:45 -0600

    set colors

--- app/helpers/color_defaults_helper.rb
+++ app/helpers/color_defaults_helper.rb
@@ -3,20 +3,31 @@ module ColorDefaultsHelper
   def grab_color(color_name)
     case color_name.to_s
     ## define our colors below:
+    # base
     when "white"
-      "#fff"
+      "white"
     when "black"
-      "#000"
-    when "lightBlue"
-      "#dfefff"
-    when "lightGrey"
-      "#bfbfbf"
-    when "grey"
-      "#ccc"
-    when "mediumGrey"
-      "#808080"
-    when "darkGrey"
+      "black"
+    # all the greys
+    when "grey1"
+      "#f1f1f1"
+    when "grey2"
+      "#e5e5e5"
+    when "grey3"
+      "#aaa"
+    when "grey4"
       "#666"
+    when "grey5"
+      "#4d4d4d"
+    when "grey6"
+      "#231f20"
+    # actual color
+    when "yellow"
+      "#ffffcc"
+    when "pink"
+      "#ffcccc"
+    when "red"
+      "#ff0000"
     end
   end
 
