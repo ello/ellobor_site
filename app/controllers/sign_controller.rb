ddb9a8b9b8245808cc91cc44545fe9b8a26ec563
gblakeman <grant.blakeman@gmail.com>
2015-06-22 14:31:33 -0600

    sign - check for existing email first

--- app/controllers/sign_controller.rb
+++ app/controllers/sign_controller.rb
@@ -5,11 +5,20 @@ class SignController < ApplicationController
   def new
     if request.xhr?
       params[:signatory][:ip_address] = request.remote_ip
-      @signatory = Signatory.new(signatory_params)
-      if @signatory.valid?
-        @signatory.save
+
+      # check for existing email / we will update & re-send verification if it exists
+      existing_signatory = Signatory.where(email: params[:signatory][:email]).first
+      if existing_signatory.blank?
+        @signatory = Signatory.new(signatory_params)
+        if @signatory.valid?
+          @signatory.save
+        else
+          @error = true
+        end
       else
-        @error = true
+        existing_signatory.update(verification_sent_at: nil, verified_at: nil)
+        @signatory = existing_signatory
+        @signatory.update(signatory_params)
       end
 
       ## set/update info to cookie to reflect previous signing
