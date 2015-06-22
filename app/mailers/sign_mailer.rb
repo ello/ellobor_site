619a05fcfcf4158366b17864cb347ce153009c10
gblakeman <grant.blakeman@gmail.com>
2015-06-22 14:36:30 -0600

    sign - email - make sure don't send to unsubscribes

--- app/mailers/sign_mailer.rb
+++ app/mailers/sign_mailer.rb
@@ -5,7 +5,7 @@ class SignMailer < ApplicationMailer
   def send_verification(signatory_id)
     @signatory = Signatory.find(signatory_id)
     if @signatory.present? && @signatory.verification_sent_at.blank?
-      unless dev_email?(@signatory.email)
+      unless dev_email?(@signatory.email) || @signatory.unsubscribed_at.present?
         @subject = "Verify Signing #{Secrets.base_site_title}"
         @signatory.update(verification_sent_at: Time.zone.now)
       else
