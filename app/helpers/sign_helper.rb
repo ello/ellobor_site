e001c86e7d23974890b543e68006d3618e0de090
gblakeman <grant.blakeman@gmail.com>
2015-06-22 15:06:48 -0600

    email - add ios + ello links

--- app/helpers/sign_helper.rb
+++ app/helpers/sign_helper.rb
@@ -4,4 +4,12 @@ module SignHelper
     "https://#{Secrets.app_url}/sign/verify/#{lookup_token}"
   end
 
+  def ello_link
+    "https://#{Secrets.parent_url}"
+  end
+
+  def ios_app_link
+    Secrets.ios_app_link
+  end
+
 end
